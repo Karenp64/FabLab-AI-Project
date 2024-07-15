dc2289cd193e1e51ecd523c94cd39dc59c3ff843
Karen Pinales <kpinales85@gmail.com>
2024-07-15 13:16:01 -0600

    Quiz3

---
+++ node_server/public/fablab-example-js.js
@@ -0,0 +1,7 @@
+<script>
+    document.getElementById("alert-button").addEventListener("click", function() {
+      document.getElementById("alert").textContent = "Alert";
+    });
+  </script>
+</body>
+
